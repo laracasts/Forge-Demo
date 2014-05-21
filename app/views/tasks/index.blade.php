6fb8fd4895c41dcbf2032e247c43293763e8f258
Jeffrey Way <jeffrey@jeffrey-way.com>
2014-05-21 18:29:48 -0400

    Change copy

--- app/views/tasks/index.blade.php
+++ app/views/tasks/index.blade.php
@@ -2,36 +2,36 @@
 
 @section('main')
 
-<h1>All Tasks</h1>
+<h1>Current Tasks</h1>
 
 <p>{{ link_to_route('tasks.create', 'Add new task') }}</p>
 
 @if ($tasks->count())
-	<table class="table table-striped table-bordered">
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Description</th>
-			</tr>
-		</thead>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+            </tr>
+        </thead>
 
-		<tbody>
-			@foreach ($tasks as $task)
-				<tr>
-					<td>{{{ $task->name }}}</td>
-					<td>{{{ $task->description }}}</td>
+        <tbody>
+            @foreach ($tasks as $task)
+                <tr>
+                    <td>{{{ $task->name }}}</td>
+                    <td>{{{ $task->description }}}</td>
                     <td>{{ link_to_route('tasks.edit', 'Edit', array($task->id), array('class' => 'btn btn-info')) }}</td>
                     <td>
                         {{ Form::open(array('method' => 'DELETE', 'route' => array('tasks.destroy', $task->id))) }}
                             {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
                         {{ Form::close() }}
                     </td>
-				</tr>
-			@endforeach
-		</tbody>
-	</table>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @else
-	There are no tasks
+    There are no tasks
 @endif
 
 @stop
